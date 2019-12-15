3d74f53b63620762c62cef61eaca1556b5a0fa9e
kozhevnikovpe <kozhevnikov-kovrov@yandex.ru>
2019-12-16 00:48:53 +0300

    hw6

--- app/src/main/java/com/pavelekozhevnikov/homework/Homework6Activity.java
+++ app/src/main/java/com/pavelekozhevnikov/homework/Homework6Activity.java
@@ -1,10 +1,12 @@
 package com.pavelekozhevnikov.homework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.content.res.Configuration;
 
 public class Homework6Activity extends AppCompatActivity {
 
@@ -14,7 +16,15 @@ public class Homework6Activity extends AppCompatActivity {
         setContentView(R.layout.activity_homework6);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        RecyclerView.LayoutManager layoutManager=null;
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            layoutManager = new GridLayoutManager(this, 2);
+
         recyclerView.setLayoutManager(layoutManager);
         Hw6Data data = new Hw6Data();
         Hw6Adapter myAdapter = new Hw6Adapter(data.getList());
